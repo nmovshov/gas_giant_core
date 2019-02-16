5bc8db9e40624791092ce44668433e0aae2bfe04
Naor Movshovitz <nmovshov@gmail.com>
2019-02-15 17:01:45 -0800

    still master

---
+++ src/planet_plotters.py
@@ -0,0 +1,20 @@
+"""A small collection of plotting methods specific to planet interior models."""
+
+from __future__ import division
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+def rho_of_s(svec, dvec):
+    """Plot density vs. normalized mean radius."""
+
+    # Some minimal input control
+
+    # Prepare the canvas
+    fh = plt.figure()
+
+    # Return the function handle
+    return fh
+
+if __name__ == '__main__':
+    print("alo world")
