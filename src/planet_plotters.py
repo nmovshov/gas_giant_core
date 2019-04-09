c5540b2c1ef52ecff1f80d0b9930bb166a88ae21
Naor Movshovitz <nmovshov@gmail.com>
2019-04-09 11:17:02 -0700

    minimal rho_of_s plot

--- src/planet_plotters.py
+++ src/planet_plotters.py
@@ -4,16 +4,28 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-def rho_of_s(svec, dvec):
+def rho_of_s(svec, dvec, *args, **kwargs):
     """Plot density vs. normalized mean radius."""
 
     # Some minimal input control
+    assert(svec.shape == dvec.shape)
 
     # Prepare the canvas
-    fh = plt.figure()
+    plt.figure()
 
-    # Return the function handle
-    return fh
+    # Prepare the data
+    x = svec/svec[0] # normalized radius
+    y = dvec/1000    # density in 1000 kg/m^3
+
+    # Plot a line
+    plt.plot(x, y, *args, **kwargs)
+
+    # Style and annotate
+    plt.xlabel('Level surface mean radius [normalized]')
+    plt.ylabel('density [1000 kg/m$^3$]')
+
+    # Display and return
+    plt.show()
 
 if __name__ == '__main__':
     print("alo world")
