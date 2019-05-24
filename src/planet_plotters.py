5870571641bed7bfa893774729cab2d377f34976
Naor Movshovitz <nmovshov@gmail.com>
2019-05-24 16:54:01 -0700

    pp.rho_of_m() implemented.

--- src/planet_plotters.py
+++ src/planet_plotters.py
@@ -3,6 +3,7 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import planet_analyzers as pa
 
 def rho_of_s(svec, dvec, *args, **kwargs):
     """Plot density vs. normalized mean radius."""
@@ -27,5 +28,32 @@ def rho_of_s(svec, dvec, *args, **kwargs):
     # Display and return
     plt.show()
 
+def rho_of_m(svec, dvec, *args, **kwargs):
+    """Plot density vs. cumulative mass."""
+
+    # Some minimal input control
+    assert(svec.shape == dvec.shape)
+
+    # Prepare the canvas
+    plt.figure()
+
+    # Prepare the data
+    m = pa.mass_variable(svec, dvec)
+    x = m/m[0]
+    y = dvec/1000 # density in 1000 kg/m^3
+
+    # Plot a line
+    plt.plot(x, y, *args, **kwargs)
+
+    # Style and annotate
+    plt.xlabel('mass variable [$M_J$]')
+    plt.ylabel('density [1000 kg/m$^3$]')
+
+    # Display and return
+    plt.show()
+
 if __name__ == '__main__':
     print("alo world")
+    import planet_generators as pg
+    svec, dvec = pg.reference_jupiter(128)
+    rho_of_m(svec, dvec)
